6675db0f4b42895c241e7560f5e82d1fd882d6e5
Jacob O'Toole <jacob.otoole1@gmail.com>
2024-03-15 21:48:19 +0000

    dotnet: WorkQueue.LightClean: add tests

---
+++ tests/dotnet-cleaner/Cleaner/Program.cs
@@ -0,0 +1,18 @@
+﻿using RedisWorkQueue;
+using FreeRedis;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        RedisClient db = new RedisClient(args[0]);
+        for (; ; )
+        {
+            string? queueName = Console.ReadLine();
+            if (queueName == null) break;
+            if (queueName == "") throw new Exception("input line must be non-empty");
+            new WorkQueue(new KeyPrefix(queueName)).LightClean(db);
+            Console.WriteLine("cleaned " + queueName);
+        }
+    }
+}
--- tests/job-spawner-and-cleaner.py
+++ tests/job-spawner-and-cleaner.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import subprocess
 from time import sleep
 
 import redis
@@ -11,17 +12,75 @@ from redis_work_queue import KeyPrefix, Item, WorkQueue
 if len(sys.argv) < 2:
     raise Exception("first command line argument must be redis host")
 host = sys.argv[1].split(":")
-queue_list_names = sys.argv[2].split(" ")
+if len(sys.argv) < 3:
+    raise Exception("second command line argument must be space-separated list of queue names (don't ask me why an argument is space separated)")
+queue_names = sys.argv[2].split(" ")
 
 db = redis.Redis(host=host[0], port=int(host[1]) if len(host) > 1 else 6379)
 if len(db.keys("*")) > 0:
     raise Exception("redis database isn't clean")
 
-shared_queue = WorkQueue(KeyPrefix("shared_jobs"))
+shared_queue_name = "shared_jobs"
+shared_queue = WorkQueue(KeyPrefix(shared_queue_name))
 queue_list = list(map(
     lambda name: WorkQueue(KeyPrefix(name)),
-    queue_list_names,
+    queue_names,
 ))
+
+def python_light_clean():
+    for queue in queue_list:
+        queue.light_clean(db)
+    shared_queue.light_clean(db)
+
+class ExternalCleaner:
+    """This wraps an external process to be used for queue cleaning.
+
+    The process should read line from stdin and write lines to stdout.
+
+    Upon reading a line, that line should be interpreted as a queue name, and that queue should be
+    cleaned. After the queue has been cleaned, the program should write to stdout "cleaned ",
+    followed by the name of the queue, followed by a newline.
+
+    The program must process the cleaning requests in the order they are sent."""
+
+    def __init__(self, command: list[str]):
+        self.child: subprocess.Popen = subprocess.Popen(
+            command,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=sys.stderr,
+            text=True,
+        )
+
+    def clean(self, queue_name: str):
+        """Request and wait for a single queue to be cleaned."""
+        if not self.check():
+            raise Exception('cleaner process is dead')
+
+        assert self.child.stdin is not None
+        self.child.stdin.write(queue_name + "\n")
+        self.child.stdin.flush()
+
+        assert self.child.stdout is not None
+        output = ""
+        while not output.startswith("cleaned "):
+            output = self.child.stdout.readline().strip()
+        assert output == "cleaned " + queue_name
+
+    def clean_all(self):
+        """Clean all the work queues."""
+        for queue_name in queue_names:
+            self.clean(queue_name)
+        self.clean(shared_queue_name)
+
+    def check(self) -> bool:
+        """Check that the process is still running."""
+        return self.child.poll() is None
+
+light_clean = python_light_clean
+if len(sys.argv) > 3 and sys.argv[3] != "":
+    # Pass the redis host to the cleaner command
+    light_clean = ExternalCleaner([sys.argv[3], sys.argv[1]]).clean_all
 
 counter = 0
 doom_counter = 0
@@ -58,9 +117,7 @@ while doom_counter < 20:
     else:
         # Otherwise, clean!
         print("Cleaning")
-        for queue in queue_list:
-            queue.light_clean(db)
-        shared_queue.light_clean(db)
+        light_clean()
     # The `doom_counter` counts the number of consecutive times all the lengths are 0.
     doom_counter = doom_counter + 1 if all(map(
         lambda queue: queue.queue_len(db) == 0 and queue.processing(db) == 0,
@@ -98,15 +155,15 @@ expecting_dict_config = {
 }
 
 
-for queue_name in queue_list_names[:]:
+for queue_name in queue_names[:]:
     if queue_name not in expecting_dict_config:
-        queue_list_names.remove(queue_name)
+        queue_names.remove(queue_name)
 
 
 keys_to_delete = []
 
 for queue_name in expecting_dict_config:
-    if queue_name not in queue_list_names:
+    if queue_name not in queue_names:
         keys_to_delete.append(queue_name)
 
 for queue_name in keys_to_delete:
@@ -152,7 +209,7 @@ for key in db.keys("*"):
             raise Exception('found unexpected key: ' + key)
 
 updated_names = []
-for name in queue_list_names:
+for name in queue_names:
     updated_names.append(name.replace("_jobs", ""))
 
 total_count_keys = 0
